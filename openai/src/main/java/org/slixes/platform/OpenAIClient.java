df5ea510fabc99f8a3ec627b15f9cfe27a7d8306
Fady Matar <fady.matar@external.bcgdv.com>
2023-09-03 08:22:36 -0700

    Added javadoc plugin

--- openai/src/main/java/org/slixes/platform/OpenAIClient.java
+++ openai/src/main/java/org/slixes/platform/OpenAIClient.java
@@ -22,6 +22,11 @@ import org.slixes.platform.openai.completion.chat.ChatCompletionRequest;
 import org.slixes.platform.openai.completion.chat.ChatCompletionResult;
 import org.slixes.platform.openai.model.Model;
 
+/**
+ * REST client interface for calling OpenAI API endpoints.
+ *
+ * @author Fady Matar
+ */
 @Path("/v1")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -29,40 +34,81 @@ import org.slixes.platform.openai.model.Model;
 @ClientHeaderParam(name = "Authorization", value = "{token}")
 public interface OpenAIClient {
 
+  /**
+   * Gets the list of models available from OpenAI.
+   *
+   * @return A Uni with the model data wrapped in a ListResponseWrapper.
+   */
   @GET
   @Path("/models")
   Uni<ListResponseWrapper<Model>> models();
 
-
+  /**
+   * Gets details for a specific OpenAI model.
+   *
+   * @param id The id of the model to get.
+   * @return A Uni with the Model data.
+   */
   @GET
   @Path("/models/{id}")
   Uni<Model> model(@PathParam("id") String id);
 
+  /**
+   * Creates a text completion for the provided request.
+   *
+   * @param request The CompletionRequest configuring the completion.
+   * @return A Uni with the CompletionResult.
+   */
   @POST
   @Path("/completions")
   Uni<CompletionResult> createCompletion(CompletionRequest request);
 
+  /**
+   * Creates a chat completion based on provided conversation history.
+   *
+   * @param request The ChatCompletionRequest with history.
+   * @return A Uni with the ChatCompletionResult.
+   */
   @POST
   @Path("/chat/completions")
   Uni<ChatCompletionResult> createChatCompletion(ChatCompletionRequest request);
 
+  /**
+   * Streams a text completion for the provided request.
+   *
+   * @param request The CompletionRequest to stream.
+   * @return A Multi emitting CompletionChunk items.
+   */
   @POST
   @Path("/completions")
   @Produces(MediaType.SERVER_SENT_EVENTS)
   @RestStreamElementType(MediaType.APPLICATION_JSON)
   Multi<CompletionChunk> createStreamedCompletion(CompletionRequest request);
 
+  /**
+   * Streams a chat completion for the provided request.
+   *
+   * @param request The ChatCompletionRequest to stream.
+   * @return A Multi emitting ChatCompletionChunk items.
+   */
   @POST
   @Path("/chat/completions")
   @Produces(MediaType.SERVER_SENT_EVENTS)
   @RestStreamElementType(MediaType.APPLICATION_JSON)
   Multi<ChatCompletionChunk> createStreamedChatCompletion(ChatCompletionRequest request);
 
-
+  /**
+   * Gets the API access token to use for authentication.
+   *
+   * @return The Bearer token string.
+   */
   default String token() {
     return "Bearer " + ConfigProvider.getConfig().getValue("openai.token", String.class);
   }
 
+  /**
+   * Response wrapper holding model data.
+   */
   record ListResponseWrapper<T>(String object, Set<T> data) {
 
   }
